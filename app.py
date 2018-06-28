caab941cb0d77816283cf76b977832b5d6570dfc
Pin Lin <moneycat711@gmail.com>
2018-06-28 14:07:45 +0800

    passers ok

--- app.py
+++ app.py
@@ -268,7 +268,6 @@ def question_number_page(number):
         logout_user()
 
     if request.method == 'GET':
-        # TODO: 顯示題目內容。
         userid = current_user.get_id()
 
         content = users[userid]['api'].show_question(number)
@@ -354,9 +353,65 @@ def question_number_passed_page(number):
     # 嘗試保持登入狀態
     if not keep_login():
         logout_user()
-    
-    # TODO: 顯示通過名單。
-    return str(users[current_user.get_id()]['api'].list_passers(number))
+    userid = current_user.get_id()
+
+    content = users[userid]['api'].show_question(number)
+
+    question_number = number
+
+    questions = {}
+    int_questions = users[userid]['api'].list_questions()
+
+    for number in int_questions:
+        if number in ext_questions:
+            questions[number] = {
+                'title': ext_questions[number]['title'],
+                'description': ext_questions[number]['description'],
+                'tag': ext_questions[number]['tag'],
+                'deadline': int_questions[number][0],
+                'submit': int_questions[number][1],
+                'status': int_questions[number][2],
+                'language': int_questions[number][3],
+                'results': users[userid]['api'].list_results(number, userid),
+            }
+        else:
+            questions[number] = {
+                'title': '未命名',
+                'description': '沒有敘述',
+                'tag': '',
+                'deadline': int_questions[number][0],
+                'submit': int_questions[number][1],
+                'status': int_questions[number][2],
+                'language': int_questions[number][3],
+                'results': users[userid]['api'].list_results(number, userid),
+            }
+
+    for number in questions:
+        if len(questions[number]['results']) == 0:
+            questions[number]['result'] = 2
+        else:
+            results = []
+            raw = questions[number]['results']
+            for result in raw:
+               results += [result[1]]
+
+            results = list(map(lambda x: x == '通過測試', results))
+
+            if False in results:
+                questions[number]['result'] = 0
+            else:
+                questions[number]['result'] = 1
+
+    question = questions[question_number]
+
+    results = []
+    raw = question['results']
+    for result in raw:
+       results += [result]
+
+    display_results = list(map(lambda x: [int(x[1] == '通過測試'), x[0], x[1]], results))
+
+    return render_template('question_number_passed.html', title=("KCOJ - " + question_number), userid=userid, gravatar=get_gravatar(users[userid]['email'], 30), question_number=question_number, question_title=question['title'], content=content, results=display_results, passers=users[current_user.get_id()]['api'].list_passers(number))
 
 # 登出沒有畫面
 @app.route('/logout', methods=['GET'], strict_slashes=False)
