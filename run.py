661a788ebd064e0d9563ea10923081517775eb73
Pin Lin <moneycat711@gmail.com>
2019-03-03 05:01:24 +0800

    Change port with environment variable

--- run.py
+++ run.py
@@ -20,7 +20,8 @@ def main():
 
     # 開啟伺服器
     from KCOJ import app
-    app.run(port=11711, threaded=True)
+    SERVER_PORT = int(os.environ.get('SERVER_PORT') or 5000)
+    app.run(port=SERVER_PORT, threaded=True)
 
 
 if __name__ == '__main__':
