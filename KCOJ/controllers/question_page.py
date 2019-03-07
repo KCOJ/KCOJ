e1c0836e21c8f0d03c7787d81f8af9268eddb77e
Pin Lin <moneycat711@gmail.com>
2019-03-07 19:55:42 +0800

    Simply question_page

--- KCOJ/controllers/question_page.py
+++ KCOJ/controllers/question_page.py
@@ -11,50 +11,30 @@ def main(useruid: str):
     user = User(useruid)
     session = get_session(useruid)
 
-    questions = {}
-
-    api_question = session.get_question()
-    for number in api_question:
-        questions[number] = {
-            'title': '未命名',
-            'description': '沒有敘述',
-            'tag': '',
-            'deadline': api_question[number]['deadline'],
-            'expired': api_question[number]['expired'],
-            'status': '已繳' if api_question[number]['status'] else '未繳',
-            'language': api_question[number]['language'],
-            'results': session.list_results(number, user.userid),
-        }
-
     closed = {}
     opened = {}
 
-    for num in questions:
-        # 判斷題目是否關閉
-        if questions[num]['expired']:
-            # 收錄至已關閉的題目
-            closed[num] = questions[num]
-            if len(closed[num]['results']) == 0:
-                # 題目燈號為未繳交
-                closed[num]['light'] = 2
-            else:
-                results = []
-                for result in closed[num]['results']:
-                    results += [result[1] == '通過測試']
-                # 題目燈號為已／未繳交
-                closed[num]['light'] = 0 if False in results else 1
+    for number, question in session.get_question().items():
+        question['title'] = "未命名"
+        question['description'] = "沒有敘述"
+        question['tag'] = ""
+        question['results'] = session.list_results(number, user.userid)
+
+        if len(question['results']) == 0:
+            # 題目燈號為未繳交
+            question['light'] = 2
         else:
-            # 收錄至仍開啟的題目
-            opened[num] = questions[num]
-            if len(opened[num]['results']) == 0:
-                # 題目燈號為未繳交
-                opened[num]['light'] = 2
-            else:
-                results = []
-                for result in opened[num]['results']:
-                    results += [result[1] == '通過測試']
-                # 題目燈號為已／未繳交
-                opened[num]['light'] = 0 if False in results else 1
+            results = []
+            for result in question['results']:
+                results += [result[1] == '通過測試']
+            # 題目燈號為已／未通過
+            question['light'] = 0 if False in results else 1
+
+        # 判斷題目是否關閉
+        if question['expired']:
+            closed[number] = question
+        else:
+            opened[number] = question
 
     return render_template(
         'question.j2',
