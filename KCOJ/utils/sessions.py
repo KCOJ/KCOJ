14cd98d74d7fb3c0fe67b883cb6ed5eb34014436
Pin Lin <moneycat711@gmail.com>
2019-03-07 10:57:04 +0800

    Create empty session

--- KCOJ/utils/sessions.py
+++ KCOJ/utils/sessions.py
@@ -5,6 +5,10 @@ from ..config import CONFIG
 sessions = {}
 
 
+def get_empty_session():
+    return KCOJ(CONFIG['TARGET']['URL'])
+
+
 def get_session(useruid: str):
     if not useruid in sessions:
         sessions[useruid] = KCOJ(CONFIG['TARGET']['URL'])
